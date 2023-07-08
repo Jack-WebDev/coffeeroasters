41285eca275739377e3ff5783419e523e01b9509
Jack-WebDev <Jackbuda31@gmail.com>
2023-07-08 16:54:30 +0200

    most of the JS functionality is working

--- JS/createPlan.js
+++ JS/createPlan.js
@@ -1,79 +1,110 @@
+
+const btn = document.getElementById("disabled");
 const accordionControls = document.querySelectorAll(".accordion__control");
 const accordionContent1 = document.getElementById("content1");
 const accordionContent2 = document.getElementById("content2");
-const spanID = document.getElementById("option1");
+const accordionContent3 = document.getElementById("content3");
+const accordionContent4 = document.getElementById("content4");
+const accordionContent5 = document.getElementById("content5");
+
+const spanID1 = document.getElementById("option1");
 const spanID2 = document.getElementById("option2");
+const spanID3 = document.getElementById("option3");
+const spanID4 = document.getElementById("option4");
+const spanID5 = document.getElementById("option5");
+
+
+// btn.disabled = false;
+
 
 accordionControls.forEach((accordionControl) => {
   const header = accordionControl.querySelector("h3");
-  // const accordionContent = accordionControl.querySelectorAll(
-  //   ".accordion__content"
-  // );
-
-  // const accordionContent1 = accordionControl.querySelector(".content1");
-  // const accordionContent2 = accordionControl.querySelector(".content2");
 
   header.addEventListener("click", () => {
     header.style.backgroundColor = "Steelblue";
-    // toggleContent(accordionContent);
   });
-
-  // console.log(accordionContent1)
-  // console.log(accordionContent2)
-  // const options1 = accordionContent1.querySelectorAll(".coffee__options");
-  // console.log(options1)
-  // options1.forEach((option1) => {
-  //   option1.addEventListener("click", () => {
-  //     const h4 = option1.querySelector("h4");
-  //     spanID.innerText = h4.innerText;
-  //   });
-  // });
-  // accordionContent.forEach((content) => {
-  //   const options = content.querySelectorAll(".coffee__option");
-  //   // console.log(content)
-
-  //   options.forEach((option) => {
-  //     // console.log(option)
-  //     const h4 = option.querySelector("h4");
-  //     // console.log(header.innerText)
-  //     option.addEventListener("click", () => {
-  //       spanID.innerText = h4.innerText;
-  //       // spanID2.innerText = h4.innerText[1];
-  //       option.style.backgroundColor = "Steelblue";
-  //     });
-  //   });
-  // });
-
-  // function toggleContent(accordionContent) {
-  //   const options = accordionContent.querySelector(".coffee__option");
-  //   options.addEventListener("click", () => {
-  //     consolXe.log("....");
-  //   });
-  // }
 });
 
-
-const content1 = accordionContent1.querySelectorAll(".coffee__option")
-const content2 = accordionContent2.querySelectorAll(".coffee__option")
+const content1 = accordionContent1.querySelectorAll(".coffee__option");
+const content2 = accordionContent2.querySelectorAll(".coffee__option");
+const content3 = accordionContent3.querySelectorAll(".coffee__option");
+const content4 = accordionContent4.querySelectorAll(".coffee__option");
+const content5 = accordionContent5.querySelectorAll(".coffee__option");
 
 // console.log(content1)
 
 content1.forEach((option) => {
-  const h4 = option.querySelector("h4")
+  const h4 = option.querySelector("h4").innerText;
 
   option.addEventListener("click", () => {
-    option.style.backgroundColor = "Steelblue";
-    spanID.innerText = h4.innerText;
-  })
-})
+
+    [...content1]
+      .filter((op) => op !== option)
+      .forEach((o) => o.classList.remove("active"));
+
+    option.classList.toggle("active");
+    spanID1.innerText = `as ${h4}`;
+  });
+});
 
 
 content2.forEach((option) => {
-  const h4 = option.querySelector("h4")
+  const h4 = option.querySelector("h4");
 
   option.addEventListener("click", () => {
-    option.style.backgroundColor = "Steelblue";
+    [...content2]
+      .filter((op) => op !== option)
+      .forEach((o) => o.classList.remove("active"));
+
+    option.classList.toggle("active");
     spanID2.innerText = h4.innerText;
-  })
-})
+  });
+});
+
+
+content3.forEach((option) => {
+  const h4 = option.querySelector("h4");
+
+  option.addEventListener("click", () => {
+    [...content3]
+      .filter((op) => op !== option)
+      .forEach((o) => o.classList.remove("active"));
+
+    option.classList.toggle("active");
+    spanID3.innerText = h4.innerText;
+  });
+});
+
+
+
+content4.forEach((option) => {
+  const h4 = option.querySelector("h4");
+
+  option.addEventListener("click", () => {
+    [...content4]
+      .filter((op) => op !== option)
+      .forEach((o) => o.classList.remove("active"));
+
+    option.classList.toggle("active");
+    spanID4.innerText = h4.innerText;
+  });
+});
+
+
+
+content5.forEach((option) => {
+  const h4 = option.querySelector("h4");
+
+  option.addEventListener("click", () => {
+    [...content5]
+      .filter((op) => op !== option)
+      .forEach((o) => o.classList.remove("active"));
+
+    option.classList.toggle("active");
+    spanID5.innerText = h4.innerText;
+  });
+});
+
+
+
 
