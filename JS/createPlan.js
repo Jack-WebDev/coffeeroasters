239d4902e34a164eb3539061329924147aa21687
Jack-WebDev <Jackbuda31@gmail.com>
2023-07-09 09:11:33 +0200

    updated home hero section to use less media queries

--- JS/createPlan.js
+++ JS/createPlan.js
@@ -1,11 +1,4 @@
-
-const btn = document.getElementById("disabled");
-const accordionControls = document.querySelectorAll(".accordion__control");
-const accordionContent1 = document.getElementById("content1");
-const accordionContent2 = document.getElementById("content2");
-const accordionContent3 = document.getElementById("content3");
-const accordionContent4 = document.getElementById("content4");
-const accordionContent5 = document.getElementById("content5");
+// const content = document.querySelectorAll(".coffee__option");
 
 const spanID1 = document.getElementById("option1");
 const spanID2 = document.getElementById("option2");
@@ -13,98 +6,104 @@ const spanID3 = document.getElementById("option3");
 const spanID4 = document.getElementById("option4");
 const spanID5 = document.getElementById("option5");
 
+function orderAccordion() {
+  const accordionControls = document.querySelectorAll(".accordion__control");
 
-// btn.disabled = false;
+  accordionControls.forEach((accordionControl) => {
+    const header = accordionControl.querySelector("h3");
 
-
-accordionControls.forEach((accordionControl) => {
-  const header = accordionControl.querySelector("h3");
-
-  header.addEventListener("click", () => {
-    header.style.backgroundColor = "Steelblue";
+    header.addEventListener("click", () => {
+      header.style.backgroundColor = "Steelblue";
+    });
   });
-});
+}
 
-const content1 = accordionContent1.querySelectorAll(".coffee__option");
-const content2 = accordionContent2.querySelectorAll(".coffee__option");
-const content3 = accordionContent3.querySelectorAll(".coffee__option");
-const content4 = accordionContent4.querySelectorAll(".coffee__option");
-const content5 = accordionContent5.querySelectorAll(".coffee__option");
+function preference() {
+  const accordionContent1 = document.getElementById("content1");
+  const content = accordionContent1.querySelectorAll(".coffee__option");
 
-// console.log(content1)
+  content.forEach((option) => {
+    const h4 = option.querySelector("h4").innerText;
+    
+    option.addEventListener("click", () => {
 
-content1.forEach((option) => {
-  const h4 = option.querySelector("h4").innerText;
+      coffeeOptions(content, option);
+      spanID1.innerText = `as ${h4}`;
 
-  option.addEventListener("click", () => {
+      return h4;
+    });
 
-    [...content1]
-      .filter((op) => op !== option)
-      .forEach((o) => o.classList.remove("active"));
-
-    option.classList.toggle("active");
-    spanID1.innerText = `as ${h4}`;
   });
-});
+  
+}
 
+function coffeeOptions(params, option) {
+  [...params]
+    .filter((op) => op !== option)
+    .forEach((o) => o.classList.remove("active"));
 
-content2.forEach((option) => {
-  const h4 = option.querySelector("h4");
+  option.classList.toggle("active");
+}
 
-  option.addEventListener("click", () => {
-    [...content2]
-      .filter((op) => op !== option)
-      .forEach((o) => o.classList.remove("active"));
+function beanType() {
+  const accordionContent2 = document.getElementById("content2");
+  const content = accordionContent2.querySelectorAll(".coffee__option");
 
-    option.classList.toggle("active");
-    spanID2.innerText = h4.innerText;
+  content.forEach((option) => {
+    const h4 = option.querySelector("h4");
+
+    option.addEventListener("click", () => {
+      coffeeOptions(content, option);
+      spanID2.innerText = h4.innerText;
+    });
   });
-});
+}
 
+function quantity() {
+  const accordionContent3 = document.getElementById("content3");
+  const content3 = accordionContent3.querySelectorAll(".coffee__option");
 
-content3.forEach((option) => {
-  const h4 = option.querySelector("h4");
+  content3.forEach((option) => {
+    const h4 = option.querySelector("h4");
 
-  option.addEventListener("click", () => {
-    [...content3]
-      .filter((op) => op !== option)
-      .forEach((o) => o.classList.remove("active"));
-
-    option.classList.toggle("active");
-    spanID3.innerText = h4.innerText;
+    option.addEventListener("click", () => {
+      coffeeOptions(content3, option);
+      spanID3.innerText = h4.innerText;
+    });
   });
-});
+}
 
+function grindOption() {
+  const accordionContent4 = document.getElementById("content4");
+  const content4 = accordionContent4.querySelectorAll(".coffee__option");
 
+  content4.forEach((option) => {
+    const h4 = option.querySelector("h4");
 
-content4.forEach((option) => {
-  const h4 = option.querySelector("h4");
-
-  option.addEventListener("click", () => {
-    [...content4]
-      .filter((op) => op !== option)
-      .forEach((o) => o.classList.remove("active"));
-
-    option.classList.toggle("active");
-    spanID4.innerText = h4.innerText;
+    option.addEventListener("click", () => {
+      coffeeOptions(content4, option);
+      spanID4.innerText = h4.innerText;
+    });
   });
-});
+}
 
+function deliveries() {
+  const accordionContent5 = document.getElementById("content5");
+  const content5 = accordionContent5.querySelectorAll(".coffee__option");
 
+  content5.forEach((option) => {
+    const h4 = option.querySelector("h4");
 
-content5.forEach((option) => {
-  const h4 = option.querySelector("h4");
-
-  option.addEventListener("click", () => {
-    [...content5]
-      .filter((op) => op !== option)
-      .forEach((o) => o.classList.remove("active"));
-
-    option.classList.toggle("active");
-    spanID5.innerText = h4.innerText;
+    option.addEventListener("click", () => {
+      coffeeOptions(content5, option);
+      spanID5.innerText = h4.innerText;
+    });
   });
-});
+}
 
-
-
-
+orderAccordion();
+console.log(preference());
+beanType();
+quantity();
+grindOption();
+deliveries();
