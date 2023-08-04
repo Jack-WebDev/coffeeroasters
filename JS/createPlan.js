e5f05626d065b50f408f243d738a72553e26ca2e
Jack <jackwebdev31@gmail.com>
2023-08-04 13:50:30 +0200

    handle choices

--- JS/createPlan.js
+++ JS/createPlan.js
@@ -1,3 +1,12 @@
+
+let question1 = '___'
+let question2 = '___'
+let question3 = '___'
+let question4 = '___'
+let question5 = '___'
+
+
+
 const choices = document.querySelectorAll("[data-option]");
 const items = document.querySelectorAll("[data-item]");
 const questions = document.querySelectorAll("[data-question]");
@@ -10,9 +19,7 @@ questions.forEach((question) => {
 
 
 choices.forEach((choice) => {
-  choice.addEventListener("click", () => {
-    console.log("Choice clicked");
-  });
+  choice.addEventListener("click", handleChoices);
 });
 
 function toggleOption(e) {
@@ -22,4 +29,44 @@ function toggleOption(e) {
 
 function handleQuestion(e) {
   const question = Number(e.currentTarget.dataset.question);
+}
+
+function handleChoices(e) {
+    const currentItem = Number(e.currentTarget.dataset.item)
+    const coffeeOption = e.currentTarget;
+    const currentOption = e.currentTarget.dataset.option;
+    // console.log(currentOption)
+
+    switch (currentItem) {
+        case 1:
+            question1 = currentOption
+            break;
+        case 2:
+            question2 = currentOption
+            break;
+        case 3:
+            question3 = currentOption
+            break;
+        case 4:
+            question4 = currentOption
+            break;
+        case 5:
+            question5 = currentOption
+            break;
+        default:
+            break;
+    }
+
+
+    let orderSummary = 
+    `"I drink my coffee ${question1 === "Capsules" ? "using" : "as"} 
+        <span>${question1}</span>, 
+        with a <span>${question2}</span> type bean. 
+        <span>${question3}</span>sent to me 
+        <span>${question5}</span>."
+    `
+
+    // console.log(question1)
+
+    document.querySelector('[data-order]').innerHTML = orderSummary;
 }
