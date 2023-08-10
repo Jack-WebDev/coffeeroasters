678fa8cdd3f08ed3fb6d541cf97e74c9f6acf0fb
Jack <jackwebdev31@gmail.com>
2023-08-10 12:42:04 +0200

    FAQ buggy

--- JS/createPlan.js
+++ JS/createPlan.js
@@ -9,40 +9,30 @@ let biweeklyPrice = 0.0;
 let monthlyPrice = 0.0;
 let totalPrice = 0.0;
 
-
 const modal = document.querySelector("#modal");
 const openModal = document.querySelector(".open__modal");
 const closeModal = document.querySelector(".close__modal");
-// console.log(closeModal)
 openModal.addEventListener("click", () => {
   modal.showModal();
-  calcPrice()
+  calcPrice();
 });
 
 closeModal.addEventListener("click", () => {
   modal.close();
 });
 
+const weeklyPrices = document.querySelector(".price__week");
+const biweeklyPrices = document.querySelector(".price__biWeek");
+const monthlyPrices = document.querySelector(".price__month");
 
-
-const weeklyPrices = document.querySelector('.price__week')
-const biweeklyPrices = document.querySelector('.price__biWeek')
-const monthlyPrices = document.querySelector('.price__month')
-// const createBtn = document.querySelector('.create-plan--btn')
-
-const totalFee = document.getElementById('totalFee')
-
+const totalFee = document.getElementById("totalFee");
 
 const choices = document.querySelectorAll("[data-option]");
 const items = document.querySelectorAll("[data-item]");
 const questions = document.querySelectorAll("[data-question]");
 
-// createBtn.addEventListener('click', createPlanBtn);
-// console.log(createBtn)
-
-
 questions.forEach((question) => {
-  question.addEventListener("click", handleQuestion);
+  // question.addEventListener("click", handleQuestion);
   question.addEventListener("click", toggleOption);
 });
 
@@ -57,13 +47,13 @@ function toggleOption(e) {
 
 function handleQuestion(e) {
   const question = Number(e.currentTarget.dataset.question);
+  question.classList.toggle("active");
 }
 
 function handleChoices(e) {
   const currentItem = Number(e.currentTarget.dataset.item);
   const coffeeOption = e.currentTarget;
   const currentOption = e.currentTarget.dataset.option;
-  // console.log(currentOption)
 
   switch (currentItem) {
     case 1:
@@ -85,36 +75,42 @@ function handleChoices(e) {
       break;
   }
 
+  // choices.forEach((choice) => {
+  //   if (currentItem === Number(choice.dataset.item)) {
+  //     console.log(choice)
+  //     choice.classList.remove("card-active");
+  //     choice.classList.add("card-hover");
+  //   }
+  // });
+  coffeeOption.classList.toggle("active");
+
   let orderSummary = `"I drink my coffee ${
-    question1 === "Capsule" ? "using" : "as"
+    question1 === "Capsules" ? "using" : "as"
   } 
         <span>${question1}</span>, 
         with a <span>${question2} </span> ${grindOption()} type bean. 
-        <span>${question3}</span>sent to me 
+        <span>${question3}</span> sent to me 
         <span>${question5}</span>."
     `;
 
-  // console.log(question3)
   capsuleOption();
   document.querySelector("[data-order]").innerHTML = orderSummary;
-  document.querySelector('.finalOrder__details').innerHTML = orderSummary
-  prices()
-  // createPlanBtn()
+  document.querySelector(".finalOrder__details").innerHTML = orderSummary;
+  prices();
 }
 
 function capsuleOption() {
-  if (question1 === "Capsule") {
+  if (question1 === "Capsules") {
     document.querySelector(".grind").style.color = "#adb5bd";
     document.querySelector(".grind").style.pointerEvents = "none";
   } else {
     document.querySelector(".grind").style.color = "#000";
     document.querySelector(".grind").style.pointerEvents = "auto";
-
   }
 }
 
 function grindOption() {
-  if (question1 === "Capsule") {
+  if (question1 === "Capsules") {
     return "";
   } else {
     return `ground ala <span id="question4">${question4}</span>`;
@@ -129,46 +125,44 @@ function prices() {
       monthlyPrice = 12.0;
       break;
     case "500g":
-      weeklyPrice = 13.00;
-      biweeklyPrice = 17.50;
-      monthlyPrice = 22.00;
+      weeklyPrice = 13.0;
+      biweeklyPrice = 17.5;
+      monthlyPrice = 22.0;
       break;
     case "1000g":
-      weeklyPrice = 22.00;
-      biweeklyPrice = 32.00;
-      monthlyPrice = 42.00;
+      weeklyPrice = 22.0;
+      biweeklyPrice = 32.0;
+      monthlyPrice = 42.0;
       break;
     default:
       break;
   }
 
-  weeklyPrices.innerText = `R${weeklyPrice.toFixed(2)} per shipment. Includes free first-class shipping.`
-  biweeklyPrices.innerText = `R${biweeklyPrice.toFixed(2)} per shipment. Includes free first-class shipping.`
-  monthlyPrices.innerText = `R${monthlyPrice.toFixed(2)} per shipment. Includes free first-class shipping.`
+  weeklyPrices.innerText = `R${weeklyPrice.toFixed(
+    2
+  )} per shipment. Includes free first-class shipping.`;
+  biweeklyPrices.innerText = `R${biweeklyPrice.toFixed(
+    2
+  )} per shipment. Includes free first-class shipping.`;
+  monthlyPrices.innerText = `R${monthlyPrice.toFixed(
+    2
+  )} per shipment. Includes free first-class shipping.`;
 }
 
 function calcPrice() {
-
   switch (question5) {
     case "Every week":
-      totalPrice = weeklyPrice * 4
+      totalPrice = weeklyPrice * 4;
       break;
     case "Every 2 weeks":
-      totalPrice = biweeklyPrice * 2
+      totalPrice = biweeklyPrice * 2;
       break;
     case "Every month":
-      totalPrice = monthlyPrice * 1
+      totalPrice = monthlyPrice * 1;
       break;
     default:
       break;
   }
 
-  totalFee.innerText = `Your total payment: ${totalPrice.toFixed(2)} / mo.`
+  totalFee.innerText = `Your total payment: ${totalPrice.toFixed(2)} / mo.`;
 }
-
-// function createPlanBtn() {
-//   modal.show()
-
-//   calcPrice()
-
-// }
